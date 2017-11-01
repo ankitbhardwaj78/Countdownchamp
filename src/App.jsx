c00dc7cb60cda032d44f7e59e230f22caaa1a4e6
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2017-11-01 18:53:24 +0530

    component css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-
+import './App.css'
 class App extends Component{
             render(){
                 return(
-          <div>
-                 <div> countdown </div>
+          <div className="App">
+                 <div className="heading"> countdown </div>
             <div>
-                 <div> 14 days</div>
-                 <div>30 hours</div>
-                 <div>15 minutes</div>
-                 <div>20 seconds</div>
+                 <div className="clock-days"> 14 days</div>
+                 <div className="clock-hours">30 hours</div>
+                 <div className="clock-minutes">15 minutes</div>
+                 <div className="clock-seconds">20 seconds</div>
            </div>
            <div>
                  <input placeholder='new date'/>
