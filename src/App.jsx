0db2df66e61d6bae339264d2d8e70ac3e332b1de
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2017-11-01 18:38:10 +0530

    component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 class App extends Component{
             render(){
                 return(
-                 <div>countdown hello</div>
+          <div>
+                 <div> countdown </div>
+            <div>
+                 <div> 14 days</div>
+                 <div>30 hours</div>
+                 <div>15 minutes</div>
+                 <div>20 seconds</div>
+           </div>
+           <div>
+                 <input placeholder='new date'/>
+                 <button>Submit </button>
+             </div>
+          </div>
                       )
                   }
           }
